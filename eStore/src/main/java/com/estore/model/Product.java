b811d2ba70f0d1591c8dea3cbb58d96a54210563
Keval Sanghavi <keval.sanghavi@gmail.com>
2017-05-20 14:15:00 -0400

    Adding admin home page, image feature, etc.

--- eStore/src/main/java/com/estore/model/Product.java
+++ eStore/src/main/java/com/estore/model/Product.java
@@ -1,70 +1,110 @@
 package com.estore.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+import org.springframework.web.multipart.MultipartFile;
+
+@Entity
 public class Product {
-	
-	private String id;
-	private String name;
-	private String category;
-	private String description;
-	private String condition;
-	private String status;
-	private String manufacturer;
-	private double price;
-	private int unitInStock;
-	
-	public String getId() {
-		return id;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long productId;
+
+	private String productName;
+	private String productCategory;
+	private String productDescription;
+	private String productCondition;
+	private String productStatus;
+	private String productManufacturer;
+	private double productPrice;
+	private int productUnitInStock;
+
+	@Transient
+	private MultipartFile productImage;
+
+	public long getProductId() {
+		return productId;
 	}
-	public void setId(String id) {
-		this.id = id;
+
+	public void setProductId(long productId) {
+		this.productId = productId;
 	}
-	public String getName() {
-		return name;
+
+	public String getProductName() {
+		return productName;
 	}
-	public void setName(String name) {
-		this.name = name;
+
+	public void setProductName(String productName) {
+		this.productName = productName;
 	}
-	public String getCategory() {
-		return category;
+
+	public String getProductCategory() {
+		return productCategory;
 	}
-	public void setCategory(String category) {
-		this.category = category;
+
+	public void setProductCategory(String productCategory) {
+		this.productCategory = productCategory;
 	}
-	public String getDescription() {
-		return description;
+
+	public String getProductDescription() {
+		return productDescription;
 	}
-	public void setDescription(String description) {
-		this.description = description;
+
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
 	}
-	public String getCondition() {
-		return condition;
+
+	public String getProductCondition() {
+		return productCondition;
 	}
-	public void setCondition(String condition) {
-		this.condition = condition;
+
+	public void setProductCondition(String productCondition) {
+		this.productCondition = productCondition;
 	}
-	public String getStatus() {
-		return status;
+
+	public String getProductStatus() {
+		return productStatus;
 	}
-	public void setStatus(String status) {
-		this.status = status;
+
+	public void setProductStatus(String productStatus) {
+		this.productStatus = productStatus;
 	}
-	public String getManufacturer() {
-		return manufacturer;
+
+	public String getProductManufacturer() {
+		return productManufacturer;
 	}
-	public void setManufacturer(String manufacturer) {
-		this.manufacturer = manufacturer;
+
+	public void setProductManufacturer(String productManufacturer) {
+		this.productManufacturer = productManufacturer;
 	}
-	public double getPrice() {
-		return price;
+
+	public double getProductPrice() {
+		return productPrice;
 	}
-	public void setPrice(double price) {
-		this.price = price;
+
+	public void setProductPrice(double productPrice) {
+		this.productPrice = productPrice;
 	}
-	public int getUnitInStock() {
-		return unitInStock;
+
+	public int getProductUnitInStock() {
+		return productUnitInStock;
 	}
-	public void setUnitInStock(int unitInStock) {
-		this.unitInStock = unitInStock;
+
+	public void setProductUnitInStock(int productUnitInStock) {
+		this.productUnitInStock = productUnitInStock;
 	}
-	
+
+	public MultipartFile getProductImage() {
+		return productImage;
+	}
+
+	public void setProductImage(MultipartFile productImage) {
+		this.productImage = productImage;
+	}
+
 }
